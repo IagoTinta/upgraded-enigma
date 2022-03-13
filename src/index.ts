762f48c2f0ecac62a6088ad101651364ad549d44
Iago Tinta <iagotinta@gmail.com>
2022-03-13 18:47:44 -0300

    Clase 2
    Ajuste renderizado de pantalla, agregue nuevos objetos e hice un contenedor

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,66 @@
-import { Application, Loader, Sprite } from 'pixi.js'
+import { Application, Container, Loader, Point, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
 window.addEventListener("resize", ()=>{
 
-	console.log("resized!");
-	app.screen.width
-	app.screen.height
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeight) / 2);
+
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
 
 })
 
-Loader.shared.add({url:"./Trifuerza.png", name:"myTriforce"});
+window.dispatchEvent(new Event("resize"));
+
+Loader.shared.add({url:"./Majora.png", name:"myMajora"});
+Loader.shared.add({url:"./Mario_Hat.png", name:"Mario_Hat"});
 
 Loader.shared.onComplete.add(()=>{
 
-	const clampy: Sprite = Sprite.from("myTriforce");
+	const Majora: Sprite = Sprite.from("myMajora");
 	
-	clampy.anchor.set(0);
-	
-	clampy.x = 0;
-	clampy.y = 0;
-	
-	app.stage.addChild(clampy);
+	Majora.anchor.set(0);
+
+	const marioHat: Sprite = Sprite.from("Mario_Hat");
+
+	marioHat.position.set(210,0);
+
+	const majoraWithHat: Container = new Container();
+
+	majoraWithHat.addChild(Majora);
+	majoraWithHat.addChild(marioHat);
+
+	majoraWithHat.scale.set(0.5);
+	majoraWithHat.position.set(200,300);
+
+	console.log(marioHat.toGlobal(new Point()));
+	console.log(marioHat.parent.toGlobal(marioHat.position));
+
+	const aux = marioHat.parent.toLocal(new Point(0,0));
+	marioHat.position.copyFrom(aux);
+
+	app.stage.addChild(majoraWithHat);
 
 });
 
